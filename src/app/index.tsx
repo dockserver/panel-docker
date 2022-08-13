84041c8415910b732db402c37410d6b62e80f9aa
doob187 <60312740+doob187@users.noreply.github.com>
2022-08-13 21:05:30 +0200

    Update index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,8 @@
-import React from "react"
+import React from "react";
 import * as ReactDOM from "react-dom";
 import { App } from "./components/app";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { HomePage } from "./pages/home";
-
 
 ReactDOM.render(
     <App>
