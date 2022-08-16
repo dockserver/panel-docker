cb00f4093325e1a33ce9f2b62026ec86bb6c2c90
doob187 <60312740+doob187@users.noreply.github.com>
2022-08-16 18:49:15 +0200

    Update index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,11 +4,15 @@ import { App } from "./components/app";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { HomePage } from "./pages/home";
 
-ReactDOM.render(
+const rootElement = document.getElementById("app");
+const root = createRoot(rootElement!);
+
+root.render(
+  <StrictMode>
     <App>
-        <Router basename={"panel"}>
-            <Route path="/" component={HomePage} />
-        </Router>
-    </App>,
-    document.getElementById("app")
+      <Router basename={"panel"}>
+         <Route path="/" component={HomePage} />
+      </Router>
+    </App>
+  </StrictMode>,
 );
